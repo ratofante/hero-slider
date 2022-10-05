b3473de5eb0ed85a3e734cc5a9d77303078c40c0
Ratofante <gwilkens.r@gmail.com>
2022-10-05 09:00:00 -0300

    hasta gsap

--- src/index.js
+++ src/index.js
@@ -6,8 +6,6 @@ document.addEventListener('DOMContentLoaded', () => {
    const searchInput = document.querySelector('#search');
 
    document.addEventListener('click', (event) => {
-      console.log(event.target.id);
-      console.log(event.target.closest('#btn-search'));
       if (event.target.id === 'btn-search') {
          searchInput.classList.toggle('active');
          searchInput.focus();
--- src/js/slider.js
+++ src/js/slider.js
@@ -1,8 +1,45 @@
 
-
-
 const slider = () => {
-   console.log('slider');
+
+   const pointers = document.querySelectorAll('.pointer');
+   const slides = document.querySelectorAll('.slide');
+
+   console.log(slides);
+
+   pointers.forEach(pointer => {
+      pointer.addEventListener('click', (e) => {
+
+         let currentSlide;
+         let newSlide;
+
+         if (!pointer.classList.contains('active')) {
+            pointers.forEach(p => {
+               if (p.classList.contains('active')) {
+                  p.classList.remove('active');
+                  currentSlide = p.getAttribute('data-pointer');
+                  console.log(currentSlide);
+               }
+               p.classList.contains('active') && p.classList.remove('active');
+            })
+            e.target.classList.add('active');
+
+            let id = e.target.getAttribute('data-pointer');
+
+            let activeSlide = () => {
+               let active;
+               slides.forEach(slide => {
+                  if (slide.classList.contains('active')) {
+                     console.log('found you!', slide);
+                     active = slide;
+                  }
+                  return active;
+               })
+            }
+
+         }
+      })
+   })
+
 }
 
 export default slider;
